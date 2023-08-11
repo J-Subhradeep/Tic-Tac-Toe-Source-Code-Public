8ec67e8d07d27f9bec5982dbfed204eaa00e06d0
Subhradeep Pal <87785813+subhradeep-rdj@users.noreply.github.com>
2023-08-11 10:27:55 +0530

    Update settings.py

--- backend/tictactoe/tictactoe/settings.py
+++ backend/tictactoe/tictactoe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rfxc#%tcj349u4ytz$wwk94*+5bd7%x%w=cb45qid)bje$_lzn'
+
 SECRET_KEY = os.environ.get('secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
